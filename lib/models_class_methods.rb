ef4c95a1b332511b3aa836a7adaef4e31c3dc2ba
Raphaël Bauduin <rb@raphinou.com>
2009-12-11 10:56:55 +0100

    added comment in models_class_methods to keep it in /lib

--- lib/models_class_methods.rb
+++ lib/models_class_methods.rb
@@ -1,3 +1,5 @@
+#this needs to be in the lib directory so that it can be loaded when ad plugin adds an observer to Instance
+#it was initially in the config/initializers directory, but caused trouble
 module ModelsClassMethods
   def self.included(base)
     base.extend ClassMethods
