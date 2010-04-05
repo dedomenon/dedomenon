bec0a75344ad919039a5ffd867fb49ae4f27ca07
Raphaël Bauduin <rb@raphinou.com>
2010-04-05 10:31:22 +0200

    moved all modules used by entities_table in an array

---
+++ config/initializers/data_table.rb
@@ -0,0 +1,5 @@
+class MadbSettings
+  def self.entities_table_modules 
+    ['substitute', 'yui2-datatable', 'madb', 'madb-tables', 'io-base', 'event-key', 'widget', 'event-custom', 'json-parse', 'anim', 'collection', 'datatype-date']
+  end
+end
